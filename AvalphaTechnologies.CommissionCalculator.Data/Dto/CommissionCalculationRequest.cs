591ffc24fb4bbc455c52ba6bbbd63e3d6ceda800
Harshal Chougule <harshalschougule@gmail.com>
2025-12-27 17:37:12 +0530

    add data layer for the project

---
+++ AvalphaTechnologies.CommissionCalculator.Data/Dto/CommissionCalculationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace AvalphaTechnologies.CommissionCalculator.Data.Dto
+{
+    public class CommissionCalculationRequest
+    {
+        public int LocalSalesCount { get; set; }
+        public int ForeignSalesCount { get; set; }
+        public decimal AverageSaleAmount { get; set; }
+    }
+}
---
+++ AvalphaTechnologies.CommissionCalculator.Data/Dto/CommissionCalculationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AvalphaTechnologies.CommissionCalculator.Data.Dto
+{
+    public class CommissionCalculationResponse
+    {
+        public decimal AvalphaTechnologiesCommissionAmount { get; set; }
+
+        public decimal CompetitorCommissionAmount { get; set; }
+    }
+}
